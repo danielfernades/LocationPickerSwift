5274be341f3c209efe58873cc7eefdee3c4a8124
Dan Rudolf <danrudolf@gmail.com>
2014-07-10 18:31:20 -0500

    Set up manual location picker in swift. Still need to experiment with Closures aka blocks for the reverse geotaging

--- LocationpickerSwift/ViewController.swift
+++ LocationpickerSwift/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
                             
+
+    @IBOutlet var mapview: MKMapView
+    @IBOutlet var setButton: UIButton
+    
+    var locationManager = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setButton.layer.cornerRadius = 5
+
+        // add pinimage to mapview **Image data is bugprone and wont run on earlier than iOS8
+        let pinImage: UIImageView = UIImageView(image: UIImage(named:"pin1"))
+        pinImage.center = (CGPointMake(self.view.center.x, self.view.center.y - pinImage.frame.height))
+        self.mapview.delegate = self
+        self.mapview.addSubview(pinImage)
+
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+
+        //New to iOS 8 - must specifiy permission type "all time" or "per use" ** plist also needs updated **
+        locationManager.requestAlwaysAuthorization()
+        
+        locationManager.startUpdatingLocation()
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onSetButtonPressed(sender: UIButton) {
+
+        var currentLocation = mapview.centerCoordinate
+        NSLog("Lon: \(currentLocation.longitude.description) Lat: \(currentLocation.latitude.description)")
+
     }
 
+    //MapView Delegate
+
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!){
+
+        NSLog(error.description)
+    }
+
+     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+
+        for location in locations{
+
+            if location.horizontalAccuracy < 150 && location.verticalAccuracy < 150 {
+                manager.stopUpdatingLocation()
+                self.setMapViewCenter()
+                NSLog(location.description)
+                break
+            }
+        }
+    }
+
+    func setMapViewCenter(){
+
+       var zoomRegion = MKCoordinateRegionMakeWithDistance(locationManager.location.coordinate, 500, 500)
+        mapview.setRegion(zoomRegion, animated:true)
+    }
 
 }
-
