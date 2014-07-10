d53b26ab978aec6dadc47607d25c166b229c6c1e
Dan Rudolf <danrudolf@gmail.com>
2014-07-10 14:52:18 -0500

    Initial Commit

---
+++ LocationpickerSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationpickerSwift
+//
+//  Created by Dan Rudolf on 7/10/14.
+//  Copyright (c) 2014 com.rudolfmedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LocationpickerSwiftTests/LocationpickerSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  LocationpickerSwiftTests.swift
+//  LocationpickerSwiftTests
+//
+//  Created by Dan Rudolf on 7/10/14.
+//  Copyright (c) 2014 com.rudolfmedia. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LocationpickerSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
